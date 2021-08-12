b73d699d47b3b23ac55ce4509ab8bbac7b36030b
Siziy Ivan <cziy@ya.ru>
2021-08-12 20:13:22 +0300

    add GraphQL

--- src/initialize.ts
+++ src/initialize.ts
@@ -1,11 +1,20 @@
 import * as sails from "typed-sails";
 import { HookTools } from "@webresto/core";
 import { resolve } from "path";
+import { setWhiteList } from "@webresto/graphql/lib/graphqlHelper"
 
 export default async function (sails: sails.default.Sails, cb) {
   // Make your code here ...
 
-  console.log("webresto-module-starter was loaded 🚀");
-  // await HookTools.default.bindModels(resolve(__dirname, "../../modelORM"));
+  console.log("webresto-module-starter just loaded 🚀");
+
+
+  // Example GRAPHQL
+  // setWhiteList({
+  //   your_model_name: ['query', 'subscription']
+  // })
+  
+  await HookTools.default.bindModels(resolve(__dirname, "../../modelORM"));
+  
   cb();
 }
