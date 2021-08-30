e7166a3a00a21b88f28ac98b06bcb5a4569243d4
Siziy Ivan <cziy@ya.ru>
2021-08-30 13:29:26 +0300

    TS model example

--- modelORM/Example.ts
+++ modelORM/Example.ts
@@ -1,8 +1,40 @@
-module.exports = {
-  attributes: {
-    name: {
-      type: 'string'
-    }
-  },  
-};
+import ORM from "@webresto/core/modelsHelp/ORM";
+import ORMModel from "@webresto/core/modelsHelp/ORMModel";
 
+let attributes = {
+  /** Id */
+  id: {
+    type: "number",
+    autoIncrement: true,
+  } as unknown as string,
+  
+  /** Name */
+  name: {
+    type: "string",
+    unique: true,
+    required: true
+  } as unknown as string,
+  
+  /** Description */
+  description: "string" as string
+}
+
+type Example = typeof attributes & ORM
+export default Example
+
+let Model  =  {
+  /** Example menhod1 */
+  async method1(attr: string): Promise<Example> {
+    return await Example.findOne();
+  }
+}
+
+module.exports = {
+  primaryKey: "id",
+  attributes: attributes,
+  ...Model
+}
+
+declare global {
+  const Example: typeof Model & ORMModel<Example>;
+}
