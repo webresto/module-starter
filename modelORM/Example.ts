8d5e514b3c282efc6d3464fbbeee79e44988442f
Siziy Ivan <cziy@ya.ru>
2021-10-02 06:00:22 +0300

    add slug in model

--- modelORM/Example.ts
+++ modelORM/Example.ts
@@ -1,40 +1,50 @@
-import ORM from "@webresto/core/modelsHelp/ORM";
-import ORMModel from "@webresto/core/modelsHelp/ORMModel";
+import ORM from "@webresto/core/interfaces/ORM";
+import ORMModel from "@webresto/core/interfaces/ORMModel";
+import slugify from "slugify"; // present in core
+import { v4 as uuid } from "uuid";
 
 let attributes = {
-  /** Id */
-  id: {
-    type: "number",
-    autoIncrement: true,
-  } as unknown as string,
-  
-  /** Name */
-  name: {
-    type: "string",
-    unique: true,
-    required: true
-  } as unknown as string,
-  
-  /** Description */
-  description: "string" as string
-}
+  title: "string",
+  slug: "string",
+  amount: "number" as unknown as number,
+};
 
 type attributes = typeof attributes;
 interface Example extends attributes, ORM {}
-export default Example
+export default Example;
 
-let Model  =  {
-  /** Example menhod1 */
-  async method1(attr: string): Promise<Example> {
-    return await Example.findOne();
-  }
-}
+let Model = {
+  async beforeCreate(init: any, proceed: any) {
+    if (!init.id) {
+      init.id = uuid();
+    }
+
+    if (!init.slug) {
+      init.slug = slugify(init.name);
+    }
+
+    // icrease 1 if slug present
+    async function getSlug(slug: string, salt?: number) {
+      let _slug = slug;
+      if (salt) _slug = slug + "-" + salt;
+
+      if (await Example.findOne({ slug: _slug })) {
+        getSlug(slug, salt + 1);
+      } else {
+        return slug + salt;
+      }
+    }
+
+    await getSlug(init.slug);
+    return proceed();
+  },
+};
 
 module.exports = {
   primaryKey: "id",
   attributes: attributes,
-  ...Model
-}
+  ...Model,
+};
 
 declare global {
   const Example: typeof Model & ORMModel<Example>;
